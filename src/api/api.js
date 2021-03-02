4e95a454eb6ab430e71e2a94d5384433419609d5
Dawid <78732041+Dawid2211@users.noreply.github.com>
2021-03-02 14:07:50 +0000

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 // Api docs: https://anapioficeandfire.com/Documentation
 
 // TODO create a function that takes a "field" argument that fetches one of:
@@ -9,5 +11,10 @@
 //  customise function (or create another function) to retrieve extra data based on configuration (number of results/pages/search
 
 // see cheat sheet for fetch example.
-export const getData = () => {
+export const getData = async (category) => {
+  const response = await fetch(
+    `https://anapioficeandfire.com/api/${category}`
+  );
+  const getDataLog = await response.json();
+    console.log(getDataLog);
 };
