62c66874ef4589a030e86973fb4c7880cb24fc49
Farida El Kafrawy <faridae@icloud.com>
2021-03-08 11:48:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
-import React from 'react';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { HeaderComponent } from './components/Header/HeaderComponent';
 import { SidebarComponent } from './components/Sidebar/SidebarComponent';
 import { FeedComponent } from './components/Feed/FeedComponent';
+import { getData } from './api/api.js';
 
 function App() {
 
+    const [getDataCategory, setGetDataCategory] = useState("");
+
+    useEffect(() => {
+        {
+            getData(getDataCategory);
+        }
+      }, [getDataCategory]);
+    
+    const changeDataCategoryToCharacters = () => {
+        setGetDataCategory("characters");
+    };
+    const changeDataCategoryToBooks = () => {
+        setGetDataCategory("books");
+    };
+    const changeDataCategoryToHouses = () => {
+        setGetDataCategory("houses");
+    };
+    
+    
     // TODO - this is the "main" component for our app, and it will include all the global state that we care about
     //  This should include things like:
     //  * the sidebar expanded state
@@ -33,13 +54,43 @@ function App() {
     // TODO [STRETCH] - implement loading state and pass to FeedComponent
 
     // TODO - pass in expanded sidebar state to components that need to know about it/update it.
-    return (
-        <div className="app">
-            <HeaderComponent />
-            <SidebarComponent />
-            <FeedComponent />
+
+document.body.style.backgroundColor = "#F3F3F3";
+document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/122977.jpg')";
+
+  return (
+
+    <div className="app">
+        <HeaderComponent />
+        <SidebarComponent />
+        <div className="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <form className="search-bar"> 
+        <input type="text" id="search" name="search" placeholder="Enter search..."/>
+        </form>
+            <button name="characters"
+            value="standard"
+            onClick={changeDataCategoryToCharacters}
+            type="submit"
+          > Characters
+          </button>
+            <button
+            value="standard"
+            onClick={changeDataCategoryToBooks}
+            type="submit"
+          > Books
+          </button>
+            <button
+            value="standard"
+            onClick={changeDataCategoryToHouses}
+            type="submit"
+          > Houses
+          </button>
         </div>
-    );
+        <FeedComponent />
+    
+    </div>
+);
 }
 
 export default App;
