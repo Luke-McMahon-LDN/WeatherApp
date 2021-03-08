dfe6e001e72a3d5820e5e509be8c0ef902a1fe8d
Farida El Kafrawy <faridae@icloud.com>
2021-03-08 13:04:14 +0000

    Create Landingpage.jsx

---
+++ src/components/Feed/Landingpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './landing.css';
+
+<div className= "landing">
+<header id="showcase">
+    <h1> Winter is coming </h1>
+    <p>Explore Game of Thrones Books, Characters and Houses</p>
+    <a href="FeedComponent.css" class="button" onclick="myFunction()" id="entersite"> Enter the site </a>
+  </header>
+  </div>
